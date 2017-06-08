06750506f2aa8e7ad22c4ec3488b0f2cd15bf426
Assan Shynybayev <shynybaev@gmail.com>
2017-06-08 20:27:57 +0600

    Added JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,16 @@
 package ru.job4j; 
 /**
 * Class Calculate решение задачи части 001 урок 1
-* @author <a href="mailto:parsentev@yandex.ru">Petr Arsentev</a>
+* @author <a href="mailto:shynybaev@gmail.com">Assan Shynybayev</a>
 * @version 2.0
-* @since 08.06.2017
+* @since 1.0
 */
 
 public class Calculate{
 
 /**
-#Метод main выводит команду на экран 
-@param args
+* Метод main выводит команду на экран 
+@param args - args
 */
   public static void main(String[] args){
 	System.out.println("Hello World");
