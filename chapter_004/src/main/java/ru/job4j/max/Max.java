b78ca861e59b447eda5c32ce708b5e724b42a79e
Assan Shynybayev <shynybaev@gmail.com>
2017-06-22 12:53:49 +0600

    Changed Max.java

--- chapter_004/src/main/java/ru/job4j/max/Max.java
+++ chapter_004/src/main/java/ru/job4j/max/Max.java
@@ -1,6 +1,9 @@
 package ru.job4j.max;
 
 public class Max {
-	public int max(int first , int second);
-		System.out.println( firs > second ? first : second );
-}  
+	public int max(int first , int second){
+	
+	System.out.println(first > second ? first : second );
+	  
+	 }  			
+}
