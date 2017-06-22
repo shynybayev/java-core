aaab92f42443bb42b19aca7c8c0f4e2fb78725d4
Assan Shynybayev <shynybaev@gmail.com>
2017-06-22 14:54:54 +0600

    Changed Calculator.java

--- chapter_003/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_003/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,45 @@
 package ru.job4j.calculator;
 
+/**
+* Class Calculate решение задачи части 001 урок 1
+* @author <a href="mailto:shynybaev@gmail.com">Assan Shynybayev</a>
+* @version 2.0
+* @since 1.0
+*/
+
 public class Calculator {
+	/*
+	* Приватное поле result
+	*/
+	
 	private double result;
 	
+	/*
+	* Метод для выполнения операции сложения
+	*/
 	public void add(double first, double second){
 		this.result = first + second;
 	}
+	
+	/*
+	* Метод для выполнения операции вычитания
+	*/
 	
 	public void substruct(double first, double second){
 		this.result = first - second;
 	}
 	
+	/*
+	* Метод для выполнения операции деления
+	*/
 	
 	public void div(double first, double second){
 		this.result = first / second;	
 	}
 	
-	
+	/*
+	* Метод для выполнения операции умножения
+	*/
  	public void multiple(double first, double second){
 		this.result = first * second;
 	}
