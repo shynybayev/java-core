2ada1c101da495defe1d0f38188c82ecfca81915
Assan Shynybayev <shynybaev@gmail.com>
2017-06-23 13:17:59 +0600

    max number

--- chapter_004/src/main/java/ru/job4j/max/Max.java
+++ chapter_004/src/main/java/ru/job4j/max/Max.java
@@ -1,9 +1,9 @@
 package ru.job4j.max;
 
-public class Max {
+public class Max{
 	public int max(int first , int second){
 	
-	System.out.println(first > second ? first : second );
+	return first > second ? first : second;
 	  
-	 }  			
+	 }
 }
