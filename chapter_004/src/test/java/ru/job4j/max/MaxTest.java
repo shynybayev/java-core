6611ed4a8d763e093329d792b537071b2c5c8255
Assan Shynybayev <shynybaev@gmail.com>
2017-06-23 13:18:44 +0600

    max number test

--- chapter_004/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_004/src/test/java/ru/job4j/max/MaxTest.java
@@ -4,28 +4,35 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+/*
+*Class Maxtest тестирует метод класса Max
+@author Assan Shynybayev
+@version 2.0
+@since 1.0
+*/
 
 public class MaxTest {
+    /*
+	*Тестирует метод max(int,int). Значение из первого параметра больше, чем второго. 
+	*/
+	@Test
+    public void whenOneOrOneThenTwo() {
+		
+		Max max = new Max();
+		int result = max.max(2, 1);
+		int expected = 2;
+		assertThat(result, is(expected));
+	}
+	/*
+	*Тестирует метод max(int,int). Значение из первого параметра меньше, чем второго.
+	*/
     @Test
-    public void whenFirstMoreThenSecond() {
-		
-		Max max = new Max();
-		max.add(2D, 1D);
-		
-		int first = first.getResult();
-		int expected = 2D;
-		assertThat(result, is(expected));
-	     
+    public void whenFirstLessThanSecond() {
+        Max max = new Max();
+        int result = max.max(1, 2);
+        int expected = 2;
+        assertThat(result, is(expected));
     }
+
 	
-	 public void whenFirstLessThenSecond() {
-		
-		Max max = new Max();
-		max.add(1D, 2D);
-		
-		int second = second.getResult();
-		int expected = 2D;
-		assertThat(result, is(expected));
-	     
-    }
 }
