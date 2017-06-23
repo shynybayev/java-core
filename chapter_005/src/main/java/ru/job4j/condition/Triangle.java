bf95974938626fcd4060140a1e5caf8554091b4a
Assan Shynybayev <shynybaev@gmail.com>
2017-06-23 17:15:44 +0600

    Added Triangle.java

--- chapter_005/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_005/src/main/java/ru/job4j/condition/Triangle.java
@@ -4,28 +4,26 @@ package ru.job4j.condition;
  * Class Triangle.
  * @author Assan Shynybayev
  * @version 2.0
- * @since 1.0
+ * @version 1.0
  */
 public class Triangle {
-    /** Вершины. */
+    /** Точки вершины Triangle*/
     private Point a, b, c;
     /**
-     * Треугольник с вершинами a, b, c.
-     * @param a вершина a
-     * @param b вершина b
-     * @param c вершина c
+     * Triangle с вершинами   a, b, c.
+     * @param a вершины a
+     * @param b вершины b
+     * @param c вершины c
      */
     public Triangle(Point a, Point b, Point c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
-    /** Площадь треугольника.
-     * @return Площадь или 0 если треугольник не существует
-     */
+    /** Метод вычисления площади Triangle.   */
     public double area() {
-        Point vectorAB = new Point(this.b.getX() - this.a.getX(), this.b.getY() - this.a.getY());
-        Point vectorAC = new Point(this.c.getX() - this.a.getX(), this.c.getY() - this.a.getY());
-        return  Math.abs(vectorAB.getX() * vectorAC.getY() - vectorAB.getY() * vectorAC.getX()) / 2;
+        Point vectorAB = new Point(this.b.getX() - this.a.getX(),this.b.getY() - this.a.getY());
+        Point vectorAC = new Point(this.c.getX() - this.a.getX(),this.c.getY() - this.a.getY());
+        return  Math.abs(vectorAB.getX()*vectorAC.getY()-vectorAB.getY()*vectorAC.getX())/2;
     }
 }
