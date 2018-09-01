edaeece7a80f7e6b9443a4f37a4feeeac228b4ca
Assan Shynybayev <mailto:shynybaev@gmail.com>
2018-09-02 03:24:22 +0600

    Add PaintTest

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test
+* @author <a href="mailto:shynybaev@gmail.com">Assan Shynybayev</a>
+* @version 1.0
+* @since 0.1
+*/
+
+public class PaintTest{
+	@Test
+	public void whenPyramidRight(){
+		Paint paint = new Paint();
+		String res = paint.rightTrl(4);
+		System.out.println(res);
+		assertThat(res, is(
+				new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+						.add("^   ")
+						.add("^^  ")
+						.add("^^^ ")
+						.add("^^^^")
+						.toString()
+				)
+			);
+		}
+	
+
+	@Test
+	public void whenPyramidLeft(){
+		Paint paint = new Paint();
+		String res = paint.leftTrl(4);
+		System.out.println(res);
+		assertThat(res, is(
+				new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+						.add("   ^")
+						.add("  ^^")
+						.add(" ^^^")
+						.add("^^^^")
+						.toString()
+		));
+	}
+}
