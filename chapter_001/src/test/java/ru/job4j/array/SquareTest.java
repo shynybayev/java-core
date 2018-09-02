93686d83f5f9e72c97518406254ac10a9bf25962
Assan Shynybayev <mailto:shynybaev@gmail.com>
2018-09-02 21:15:22 +0600

    Add arrays test

---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test
+* @author <a href="mailto:shynybaev@gmail.com">Assan Shynybayev</a>
+* @version 1.0
+* @since 0.1
+*/
+
+public class SquareTest{
+
+	@Test
+	public  void  whenBound3Then149() {
+		int bound = 3;
+		Square sq = new Square();
+		int[] res = sq.calculate(bound);
+		int[] expected = new  int[]{1, 4, 9};
+		assertThat(res, is(expected));
+	}
+
+}
