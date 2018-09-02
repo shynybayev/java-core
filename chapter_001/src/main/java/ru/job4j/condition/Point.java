7177d92c5da59d1e4afe68eb0430d6d629262e2f
Assan Shynybayev <mailto:shynybaev@gmail.com>
2018-09-02 17:24:24 +0600

    corrected

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -30,13 +30,4 @@ public class Point{
 	public double distanceTo(Point that){
 		return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
 	}
-
-
-	public static void main(String[] args) {
-		Point a = new Point(0, 1);
-		Point b = new Point(2, 5);
-		double result = a.distanceTo(b);
-		System.out.println("Расстояние между точками А и В : " + result);
-	}
-
 }
