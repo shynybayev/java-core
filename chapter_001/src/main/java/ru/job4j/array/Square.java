de15fff3995c5e317ca6d777d5e31cc9384b1efb
Assan Shynybayev <mailto:shynybaev@gmail.com>
2018-09-02 21:14:51 +0600

    Add array

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+* Class Square
+* @author <a href="mailto:shynybaev@gmail.com">Assan Shynybayev</a>
+* @version 1.0
+* @since 0.1
+*/
+
+public class Square{
+	public int[] calculate(int bound){
+		int[] rst = new int[bound];
+		int index = 1;
+ 		for (int i = 0; i < bound; i++ ) {
+			rst[i] = (int) Math.pow(index++, 2);
+		}
+		return rst;
+	}
+
+}
