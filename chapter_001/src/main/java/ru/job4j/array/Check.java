399319b572fb904c980a9e48de905b9fbce82eda
Assan Shynybayev <mailto:shynybaev@gmail.com>
2018-09-03 16:01:00 +0600

    was corrected

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -10,14 +10,12 @@ package ru.job4j.array;
 public class Check{
 
 	public boolean mono(boolean[] data){
-		boolean result = false;
+		boolean result = true;
 
 		for (int i = 0; i < data.length; i++) {
 			if (data[0] != data[i] ) {
 				result = false;
 				break;
-			} else {
-				result = true;
 			}
 		}
 		return result;
