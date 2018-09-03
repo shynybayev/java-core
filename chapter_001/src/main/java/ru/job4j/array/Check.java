a660b24222e1e8abe4958699655d0557f01531ba
Assan Shynybayev <mailto:shynybaev@gmail.com>
2018-09-03 15:22:07 +0600

    corrected

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -13,7 +13,7 @@ public class Check{
 		boolean result = false;
 
 		for (int i = 0; i < data.length; i++) {
-			if (data[0] ^ data[i] ) {
+			if (data[0] != data[i] ) {
 				result = false;
 				break;
 			} else {
