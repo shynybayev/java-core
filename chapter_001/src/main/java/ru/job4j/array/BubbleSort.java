692f412a1cda3eaacdec39b62ba4464638916fad
Assan Shynybayev <mailto:shynybaev@gmail.com>
2018-09-03 17:57:28 +0600

    add BubbleSort task

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+* Class BubbleSort
+* @author <a href="mailto:shynybaev@gmail.com">Assan Shynybayev</a>
+* @version 1.0
+* @since 0.1
+*/
+
+public class BubbleSort{
+
+	public int[] sort(int[] array){
+		for (int i = 0; i < array.length ; i++) {
+			for (int j = 0; j < i ; j++) {
+				if (array[i] < array[j]) {
+					int temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+
+			}
+		}
+		return array;
+	}
+
+	public static void main(String[] args) {
+		BubbleSort bubbleSort = new BubbleSort();
+		System.out.println(Arrays.toString(bubbleSort.sort(new int[]{2,6,4,3})));
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test
+* @author <a href="mailto:shynybaev@gmail.com">Assan Shynybayev</a>
+* @version 1.0
+* @since 0.1
+*/
+
+public class BubbleSortTest {
+	
+	@Test
+	public void whenSortAnArray(){
+		BubbleSort bubbleSort = new BubbleSort();
+		int[] input = new int[]{2, 4, 1, 5, 3};
+		int[] result = bubbleSort.sort(input);
+		int[] expected = new int[]{1, 2, 3, 4, 5};
+		assertThat(result, is(expected));
+	}
+
+}
