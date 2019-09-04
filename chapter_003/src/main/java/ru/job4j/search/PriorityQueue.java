8feaefeed7247ed80afcf34bf1e79a608b1a81ae
Assan <shynybaev@gmail.com>
2019-09-04 09:48:11 +0600

    Fix the method put

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -5,14 +5,16 @@ import java.util.LinkedList;
 public class PriorityQueue {
 
     private LinkedList<Task> tasks = new LinkedList<>();
+    private int count = 0;
     /**
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определяет по полю приоритет.
      * @param task задача
      */
     public void put(Task task) {
-        tasks.add(task);
+        tasks.add(count++, task);
         while (tasks.get(0).getPriority() > task.getPriority()) {
+             tasks.removeFirst();
              tasks.addFirst(task);
         }
     }
